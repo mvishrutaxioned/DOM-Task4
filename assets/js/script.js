8b3ded51f6b6b9f2099dedc5c047bd1d85fa7ca7
VishrutMargam <vishrutm@axioned.com>
2021-11-30 14:58:32 +0530

    added first name functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,4 +6,22 @@ $(document).ready(function() {
     var newData;
     var myData;
     var edit;
+
+    // checkout first name function
+    function checkFName(f, obj) {
+        var i = $('.fName')
+        var regex = /[^a-zA-z]/g;
+        if($('#firstName').val() == '') {
+            var msg = 'First name field is required';
+            displayError(i, msg)
+            return false;
+        } else if (regex.test($('#firstName').val())){
+            var msg = 'Please enter valid first name';
+            displayError(i, msg);
+        } else {
+            displaySuccess(i)
+            f.f = true
+            obj.firstName = $('#firstName').val()
+        }
+    }
 })
